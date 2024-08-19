ef67c0caa265bee33141d520f83b527361a59bf6
LinMihigo <linmihigo@gmail.com>
2024-08-19 16:53:58 +0200

    Add tokenizing function to header file

--- shell.h
+++ shell.h
@@ -6,8 +6,12 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <string.h>
+
+#define TOKEN_ARRAY_SIZE 20
 
 void display_prompt(void);
-void get_input(char *lineptr, size_t len);
+char *get_input(char *lineptr, size_t len);
+void string_tok(char *str, char *delim);
 
 #endif
