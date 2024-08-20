6f1f034067d459bc35784cabc8d0c7018aa598d7
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-08-20 11:09:10 +0000

    Add execute function call and add main function arguments

--- shell.c
+++ shell.c
@@ -2,21 +2,30 @@
 
 /**
  * main - Shell program entry point
+ * @argc: Number of commandline arguments
+ * @argv: Pointer to array of commandline arguments
+ * @envp: Pointer to array of environment variables
  *
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int main(int argc, char *argv[], char *envp[])
 {
 	char input[128];
-	char *delim = " ";
+	char *delim = DELIM;
 	char *str;
+	char *pathname;
+	char **args;
+	(void)argc;
+	(void)argv;
 
 	while (1)
 	{
 		display_prompt();
 		str = get_input(input, sizeof(input));
-		string_tok(str, delim);
+		args = string_tok(str, delim);
+		pathname = args[0];
+		execute(pathname, args, envp);
 	}
 	free(input);
 
