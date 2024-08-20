971309e33c3b58802bd9041e7acfb488769f9eb7
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-08-20 11:10:07 +0000

    Add execute function prototype, add new string_tok function prototype

--- shell.h
+++ shell.h
@@ -7,11 +7,14 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
+#include <sys/wait.h>
 
 #define TOKEN_ARRAY_SIZE 20
+#define DELIM " \t\n"
 
 void display_prompt(void);
 char *get_input(char *lineptr, size_t len);
-void string_tok(char *str, char *delim);
+char **string_tok(char *str, char *delim);
+void execute(char *pathname, char *args[], char *envp[]);
 
 #endif
