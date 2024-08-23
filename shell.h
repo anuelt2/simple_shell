8ef69521aca03f5c3c9730a2301fd7f52b7c0372
LinMihigo <linmihigo@gmail.com>
2024-08-23 21:44:22 +0200

    Update header file to accomodate newly added functions

--- shell.h
+++ shell.h
@@ -8,6 +8,8 @@
 #include <fcntl.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define TOKEN_ARRAY_SIZE 20
 #define DELIM " \t\n"
@@ -16,5 +18,10 @@ void display_prompt(void);
 char *get_input(char *lineptr, size_t len);
 char **string_tok(char *str, char *delim);
 void execute(char *pathname, char *args[], char *envp[]);
+char *get_env_path(char **envp, int size);
+int _path_size(char **envp);
+char *find_command(char *pathname, char **envp);
+char *_concatenate(char *dir, char *pathname);
+int is_executable(char *full_path);
 
 #endif
