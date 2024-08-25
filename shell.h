83bb6cf7fffe29b3f5349a9422303f6fedd9f4f8
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-08-25 02:24:45 +0000

    Add find_ext_file and execute_builtin and execute_external function prototypes

--- shell.h
+++ shell.h
@@ -17,11 +17,13 @@
 void display_prompt(void);
 char *get_input(char *lineptr, size_t len);
 char **string_tok(char *str, char *delim);
-void execute(char *pathname, char *args[], char *envp[]);
+void execute_builtin(char *args[]);
+void execute_external(char *pathname, char *args[], char *envp[]);
 char *get_env_path(char **envp, int size);
 int _path_size(char **envp);
 char *find_command(char *pathname, char **envp);
 char *_concatenate(char *dir, char *pathname);
 int is_executable(char *full_path);
+char *find_ext_file(char *pathname, char **envp);
 
 #endif
