eaae13b9a85402606e9a95d34232e7a45b040a47
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-08-25 10:40:33 +0000

    Modify function prototypes for modified functions

--- shell.h
+++ shell.h
@@ -17,13 +17,13 @@
 void display_prompt(void);
 char *get_input(char *lineptr, size_t len);
 char **string_tok(char *str, char *delim);
-void execute_builtin(char *args[]);
-void execute_external(char *pathname, char *args[], char *envp[]);
+void exec_builtin(char *args[]);
+void exec_external(char *pathname, char *args[], char *envp[], int cmd_count);
 char *get_env_path(char **envp, int size);
 int _path_size(char **envp);
 char *find_command(char *pathname, char **envp);
 char *_concatenate(char *dir, char *pathname);
 int is_executable(char *full_path);
-char *find_ext_file(char *pathname, char **envp);
+char *find_ext_file(char *pathname, char **envp, int cmd_count);
 
 #endif
