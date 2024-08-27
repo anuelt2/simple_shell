2dfcbfdf1b0ad30a2ef125f4a5723e2eb9721cee
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-08-27 12:54:40 +0000

    Rename files

---
+++ home_path_functions.c
@@ -0,0 +1,56 @@
+#include "shell.h"
+
+/**
+* home_path_size - Get home path size
+* @envp: Environment variable
+*
+* Return: Size of path
+*/
+
+int home_path_size(char **envp)
+{
+	int i = 0, j = 4;
+
+	while (envp[i] != NULL)
+	{
+		if (envp[i][0] == 'H' && envp[i][1] == 'O' &&
+			envp[i][2] == 'M' && envp[i][3] == 'E')
+		{
+			while (envp[i][j++] != '\0')
+			;
+		}
+		i++;
+	}
+
+	return (j);
+}
+
+/**
+* get_home_path - Get the home path
+* @envp: Environment variable
+* @size: Size of environment path variable
+*
+* Return: Pointer to path variable
+*/
+
+char *get_home_path(char **envp, int size)
+{
+	char *path;
+	int i = 0, j = 4, k = 0;
+
+	size = home_path_size(envp);
+	path = malloc(sizeof(char) * (size + 1));
+	path[size] = '\0';
+	while (envp[i] != NULL)
+	{
+		if (envp[i][0] == 'H' && envp[i][1] == 'O'
+			&& envp[i][2] == 'M' && envp[i][3] == 'E')
+		{
+			while (envp[i][j++] != '\0')
+				path[k++] = envp[i][j];
+		}
+		i++;
+	}
+
+	return (path);
+}
