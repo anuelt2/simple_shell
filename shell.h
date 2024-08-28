b4274b2361221964f583e27c541bb0c8999b4b18
LinMihigo <linmihigo@gmail.com>
2024-08-29 01:16:37 +0200

    Update header file

--- shell.h
+++ shell.h
@@ -56,7 +56,8 @@ int is_executable(char *full_path);
 int env_size(void);
 int get_env_size(char **env);
 char **copy_environ(void);
-int env_func_caller(char **args);
+char *get_env_var_value(char **env, char *var);
+int get_env_var_value_len(char **env, char *var);
 
 
 /* env_functions */
