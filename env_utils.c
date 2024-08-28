bce1bcd4a2efc70343a49f574efbbc8b303812eb
LinMihigo <linmihigo@gmail.com>
2024-08-29 01:18:42 +0200

    Add more helper functions

--- env_utils.c
+++ env_utils.c
@@ -30,6 +30,55 @@ int get_env_size(char **env)
 }
 
 /**
+* get_env_var_value_len - get an env var's value
+* @env: environment list
+* @var: variable name
+* Return: pointer to this value
+*/
+int get_env_var_value_len(char **env, char *var)
+{
+	int i, j, var_len = _strlen(var), n = 0;
+
+	for (i = 0; env[i] != NULL; i++)
+	{
+		if (_strncmp(env[i], var, var_len) == 0 && env[i][var_len] == '=')
+		{
+			for (j = var_len; env[i][j] != '\0'; j++)
+			{
+				n++;
+			}
+		}
+	}
+	return (n);
+}
+
+/**
+* get_env_var_value - get an env var's value
+* @env: environment list
+* @var: variable name
+* Return: pointer to this value
+*/
+char *get_env_var_value(char **env, char *var)
+{
+	int i, j, k, var_len = _strlen(var);
+	char *value;
+	int var_val_len = get_env_var_value_len(env, var);
+
+	value = malloc(sizeof(char) * (var_val_len + 1));
+	if (!value)
+		exit(EXIT_FAILURE);
+	value[var_val_len] = '\0';
+	for (i = 0; env[i] != NULL; i++)
+	{
+		if (_strncmp(env[i], var, var_len) == 0 && env[i][var_len] == '=')
+		{
+			for (j = var_len, k = 0; env[i][j] != '\0'; j++, k++)
+				value[k] = env[i][j];
+		}
+	}
+	return (value);
+}
+/**
 * copy_environ - create a copy of environ
 * Return: pointer to copy of environ
 */
@@ -53,29 +102,4 @@ char **copy_environ(void)
 		environ_copy[i][env_var_len] = '\0';
 	}
 	return (environ_copy);
-}
-
-/**
-* env_func_caller - set which env to call
-* @args: arrays of args
-* Return: 0 - success, 1 - failure
-*/
-int env_func_caller(char **args)
-{
-	if (_strcmp(args[0], "env") == 0)
-	{
-		get_env();
-		return (0);
-	}
-	else if (_strcmp(args[0], "setenv") == 0)
-	{
-		set_env(args[1], args[2], 1);
-		return (0);
-	}
-	else if (_strcmp(args[0], "unsetenv") == 0)
-	{
-		unset_env(args[1]);
-		return (0);
-	}
-	return (1);
 }
