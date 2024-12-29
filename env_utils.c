84f824670a105afa800ec076183a2b6fbc4a454a
LinMihigo <linmihigo@gmail.com>
2024-12-29 16:45:54 +0200

    Fix: optimised some functions to reduce redudant code, also to use glob struct instead of global variables

--- env_utils.c
+++ env_utils.c
@@ -1,105 +1,97 @@
 #include "shell.h"
 
 /**
-* env_size - get environ size
-* Return: length of environ
-*/
+ * env_size - get environ size
+ * Return: length of environ
+ */
 int env_size(void)
 {
 	int i;
 
 	for (i = 0; environ[i] != NULL; i++)
-	;
+		;
 
 	return (i);
 }
 
 /**
-* get_env_size - get environ size
-* @env: environment variable size to measure
-* Return: length of env list size
-*/
+ * get_env_size - get environ size
+ * @env: environment variable size to measure
+ * Return: length of env list size
+ */
 int get_env_size(char **env)
 {
 	int i;
 
 	for (i = 0; env[i] != NULL; i++)
-	;
+		;
 
 	return (i);
 }
 
 /**
-* get_env_var_value_len - get an env var's value
-* @env: environment list
-* @var: variable name
-* Return: pointer to this value
-*/
+ * get_env_var_value_len - get an env var's value
+ * @env: environment list
+ * @var: variable name
+ * Return: pointer to this value
+ */
 int get_env_var_value_len(char **env, char *var)
 {
-	int i, j, var_len = _strlen(var), n = 0;
+	int n = 0;
+	size_t i, j, var_len = _strlen(var);
 
 	for (i = 0; env[i] != NULL; i++)
 	{
 		if (_strncmp(env[i], var, var_len) == 0 && env[i][var_len] == '=')
 		{
 			for (j = var_len; env[i][j] != '\0'; j++)
-			{
 				n++;
-			}
 		}
 	}
 	return (n);
 }
 
 /**
-* get_env_var_value - get an env var's value
-* @env: environment list
-* @var: variable name
-* Return: pointer to this value
-*/
+ * get_env_var_value - get an env var's value
+ * @env: environment list
+ * @var: variable name
+ * Return: pointer to this value
+ */
 char *get_env_var_value(char **env, char *var)
 {
-	int i, j, k, var_len = _strlen(var);
-	char *value;
-	int var_val_len = get_env_var_value_len(env, var);
+	int i;
+	size_t var_len = _strlen(var);
 
-	value = malloc(sizeof(char) * (var_val_len + 1));
-	if (!value)
-		exit(EXIT_FAILURE);
-	value[var_val_len] = '\0';
 	for (i = 0; env[i] != NULL; i++)
 	{
 		if (_strncmp(env[i], var, var_len) == 0 && env[i][var_len] == '=')
 		{
-			for (j = var_len, k = 0; env[i][j] != '\0'; j++, k++)
-				value[k] = env[i][j];
+			return (_strdup(env[i] + var_len + 1));
 		}
 	}
-	return (value);
+	return (NULL);
 }
 /**
-* copy_environ - create a copy of environ
-* Return: pointer to copy of environ
-*/
+ * copy_environ - create a copy of environ
+ * Return: pointer to copy of environ
+ */
 char **copy_environ(void)
 {
-	char **environ_copy;
 	int i, j, env_var_len, environ_size = env_size();
 
-	environ_copy = malloc(sizeof(char *) * (environ_size + 1));
-	if (!environ_copy)
+	glob.environ_copy = malloc(sizeof(char *) * (environ_size + 1));
+	if (!glob.environ_copy)
 		exit(EXIT_FAILURE);
-	environ_copy[environ_size] = NULL;
+	glob.environ_copy[environ_size] = NULL;
 	for (i = 0; environ[i] != NULL; i++)
 	{
 		env_var_len = _strlen(environ[i]);
-		environ_copy[i] = malloc(sizeof(char) * (env_var_len + 1));
-		if (!environ_copy[i])
+		glob.environ_copy[i] = malloc(sizeof(char) * (env_var_len + 1));
+		if (!glob.environ_copy[i])
 			exit(EXIT_FAILURE);
 		for (j = 0; environ[i][j] != '\0'; j++)
-			environ_copy[i][j] = environ[i][j];
-		environ_copy[i][env_var_len] = '\0';
+			glob.environ_copy[i][j] = environ[i][j];
+		glob.environ_copy[i][env_var_len] = '\0';
 	}
-	return (environ_copy);
+	return (glob.environ_copy);
 }
