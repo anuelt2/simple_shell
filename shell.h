abad3afa6c4128a7bd0514d362794619da5bd80c
LinMihigo <linmihigo@gmail.com>
2024-12-29 18:22:19 +0200

    Add structs & etc to avoid having to use global variables

--- shell.h
+++ shell.h
@@ -11,61 +11,105 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <limits.h>
+#include <signal.h>
+
+/**
+* * Task 0 - 4 pass tests
+* * Task 10 passes tests
+* TODO-1: env/setenv/unsetenv might be ok. (Need to submit and see)
+* TODO-2: implement getline and strtok
+* TODO-3: Handling comments, files and variables
+*/
 
 #define TOKEN_ARRAY_SIZE 20
 #define DELIM " \t\n"
 
+/* Global variables */
+
+/**
+* struct Global - Holds globally used variables
+* @environ_copy: copy of environ
+* @input: glob.input from command line
+* @comm_path: path of current command
+*/
+struct Global
+{
+	char **environ_copy;
+	char *input;
+	char *comm_path;
+} glob;
+
+/* shell.c */
+void handle_sigint(int sig);
+
+/* prompt.c */
 void display_prompt(void);
-char *get_input(char *lineptr, size_t len);
-char **string_tok(char *str, char *delim);
+
+/* get_input.c */
+void get_input(void);
+
+/* _free.c */
+void _free(void **ptr);
+void free_resources(char **args);
+
+/* execute.c */
 int exec_builtin(char *args[], char **envp);
-void exec_external(char *pathname, char *args[], char *envp[], int cmd_count);
-char *get_env_path(char **envp, int size);
+void exec_external(char *comm, char *args[], char *envp[], int cmd_count);
+
+/* exec_utils.c */
 int _path_size(char **envp);
-char *find_command(char *pathname, char **envp);
-char *_concatenate(char *dir, char *pathname);
-int is_executable(char *full_path);
-char *find_ext_file(char *pathname, char **envp, int cmd_count);
+char *get_env_path(char **envp, int size);
+char *_concatenate(char *dir, char *comm);
+char *find_ext_file(char *comm, char **envp);
+
+/* home_path_functions.c */
 char *get_home_path(char **envp, int size);
+
 int home_path_size(char **envp);
+
+/* pwd_path_size */
 char *get_pwd_path(char **envp, int size);
 int pwd_path_size(char **envp);
 char *get_oldpwd_path(char **envp, int size);
 int oldpwd_path_size(char **envp);
+
+/* cd_functions */
 int cd_args(char **args, char **envp);
+
+/* exit.c */
 void exit_function(char *args[]);
+
+/* cd_args.c */
 int set_oldpwd(void);
 int set_pwd(void);
 int cd_exec(char *args[], char **envp);
 
 /* str_utils.c */
-int _strlen(char *str);
+size_t _strlen(char *str);
 int _strcmp(char *str1, char *str2);
-int _strncmp(char *str1, char *str2, int n);
+size_t _strncmp(char *str1, char *str2, size_t n);
 char *_concat(char *var, char *val);
 int _strcon(char *str, char c);
 
-/* exec_utils */
-char *get_env_path(char **envp, int size);
-int _path_size(char **envp);
-char *find_command(char *pathname, char **envp);
-char *_concatenate(char *dir, char *pathname);
-int is_executable(char *full_path);
+/* str_utils2.c */
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *s);
+char *_strcat(char *dest, char *src);
+char **string_tok(char *str, char *delim);
+char *format_env_var(char *name, char *value);
 
 /* env_utils.c */
 int env_size(void);
 int get_env_size(char **env);
-char **copy_environ(void);
-char *get_env_var_value(char **env, char *var);
 int get_env_var_value_len(char **env, char *var);
-
+char *get_env_var_value(char **env, char *var);
+char **copy_environ(void);
 
 /* env_functions */
 extern char **environ;
 void get_env(void);
 int set_env(char *var, char *val, int overwrite);
 int unset_env(char *var);
-
-char *shell_path_exec(char *pathname, char *full_path);
+int env_function_caller(char **args);
 
 #endif
