9b09f9b7bf89497e83801ea0f95b79b34758ad37
LinMihigo <linmihigo@gmail.com>
2024-12-29 18:19:30 +0200

    Add proper error handling for when command is not found, and update functions to not use global variables

--- execute.c
+++ execute.c
@@ -17,22 +17,8 @@ int exec_builtin(char *args[], char **envp)
 		if (strcmp(args[0], "cd") == 0)
 		{
 			cd_exec(args, envp);
+			_free((void **)&glob.input);
 			return (1);
-		}
-		if (_strcmp(args[0], "env") == 0)
-		{
-			get_env();
-			return (0);
-		}
-		else if (_strcmp(args[0], "setenv") == 0)
-		{
-			set_env(args[1], args[2], 1);
-			return (0);
-		}
-		else if (_strcmp(args[0], "unsetenv") == 0)
-		{
-			unset_env(args[1]);
-			return (0);
 		}
 	}
 	return (0);
@@ -40,7 +26,7 @@ int exec_builtin(char *args[], char **envp)
 
 /**
  * exec_external - Executes external commands
- * @pathname: Program to execute
+ * @comm: Program to execute
  * @args: Array of commandline arguments
  * @envp: Pointer to array of environment variables
  * @cmd_count: Count of commands entered in each shell session
@@ -48,32 +34,41 @@ int exec_builtin(char *args[], char **envp)
  * Return: Void
  */
 
-void exec_external(char *pathname, char *args[], char *envp[], int cmd_count)
+void exec_external(char *comm, char *args[], char *envp[], int cmd_count)
 {
 	pid_t child_pid;
 	int status;
-	char *full_path;
 
-	full_path = find_ext_file(pathname, envp, cmd_count);
-	if (is_executable(full_path))
+	find_ext_file(comm, envp);
+	if (glob.comm_path)
 	{
 		child_pid = fork();
 		if (child_pid == -1)
 		{
 			perror("Fork Error");
-			exit(EXIT_FAILURE);
+			_free((void **)&glob.comm_path);
+			printf("child_process = -1 - Double free\n");
+			exit_function(args);
 		}
 		else if (child_pid == 0)
 		{
-			if ((execve(full_path, args, envp)) == -1)
+			if ((execve(glob.comm_path, args, envp) == -1))
 			{
-				perror("./shell");
-				exit(EXIT_FAILURE);
+				perror("execve");
+				_free((void **)&glob.comm_path);
+				printf("child_process = 0 - Double free\n");
+				exit_function(args);
 			}
 		}
 		else
 		{
 			wait(&status);
+			_free((void **)&glob.comm_path);
 		}
+	}
+	else
+	{
+		fprintf(stderr, "simple_shell: %d: %s: not found\n", cmd_count, comm);
+		_free((void **)&glob.comm_path);
 	}
 }
