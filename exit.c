aed1ed06922e243120a521a3def1d135fc56bd23
LinMihigo <linmihigo@gmail.com>
2024-12-29 18:20:41 +0200

    Fix: optimised how memory is freed on exiting shell

--- exit.c
+++ exit.c
@@ -12,12 +12,26 @@ void exit_function(char *args[])
 	int status;
 
 	if (args[1] != NULL)
-	{
 		status = atoi(args[1]);
-		exit(status);
-	}
 	else
-	{
-		exit(0);
-	}
+		status = 0;
+	if (args != NULL)
+		free_resources(args);
+	if (glob.environ_copy)
+		free_resources(glob.environ_copy);
+	if (glob.input)
+		_free((void **)&glob.input);
+	exit(status);
+}
+
+/**
+ * handle_sigint - handle exit at Ctrl + C
+ * @sig: Int representing the signal that was caught by the OS
+ * Return: void
+ */
+void handle_sigint(int sig)
+{
+	free_resources(glob.environ_copy);
+	_free((void **)&glob.input);
+	exit(sig);
 }
