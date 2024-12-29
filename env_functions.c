9124ad04625d285b11f5b5a95d4a0841337085e5
LinMihigo <linmihigo@gmail.com>
2024-12-29 16:44:26 +0200

    Update functions to use structs instead global variables

--- env_functions.c
+++ env_functions.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
-* get_env - print env variables
-* Return: void
-*/
+ * get_env - print env variables
+ * Return: void
+ */
 void get_env(void)
 {
 	int i, j;
@@ -19,74 +19,73 @@ void get_env(void)
 }
 
 /**
-* set_env - Initialise new env var or modify an existing one
-* @var: variable name to create or modify
-* @val: value to give to pass to var
-* @overwrite: instruction to overwrite existing variable
-* Return: int
-*/
+ * set_env - Initialise new env var or modify an existing one
+ * @var: variable name to create or modify
+ * @val: value to give to pass to var
+ * @overwrite: instruction to overwrite existing variable
+ * Return: int
+ */
 int set_env(char *var, char *val, int overwrite)
 {
-	char **environ_copy = copy_environ(), **temp;
-	int i, var_len;
+	int i, var_len, environ_copy_size;
 	char *new_var = _concat(var, val);
-	int environ_copy_size = get_env_size(environ_copy);
+
+	if (!glob.environ_copy)
+		copy_environ();
+	environ_copy_size = get_env_size(glob.environ_copy);
 
 	if (!var && _strcon(var, '=') != 0)
 		return (-1);
 
 	var_len = _strlen(var);
-	for (i = 0; environ_copy[i] != NULL; i++)
+	for (i = 0; glob.environ_copy[i] != NULL; i++)
 	{
-		if (_strncmp(environ_copy[i], var, var_len) == 0 &&
-			environ_copy[i][var_len] == '=')
+		if (_strncmp(glob.environ_copy[i], var, var_len) == 0 &&
+			glob.environ_copy[i][var_len] == '=')
 		{
 			if (overwrite == 0)
 			{
 				return (0);
 			}
-			printf("%d\n", i);
 			break;
 		}
 	}
 
-	if (environ_copy[i] != NULL)
+	if (glob.environ_copy[i] != NULL)
 	{
-		environ_copy[i] = new_var;
+		glob.environ_copy[i] = new_var;
 	}
 	else
 	{
-		temp = realloc(environ_copy, sizeof(char *) * (environ_copy_size + 2));
-		if (!temp)
+		glob.environ_copy = realloc(glob.environ_copy, sizeof(char *) *
+		(environ_copy_size + 2));
+		if (!glob.environ_copy)
 			return (-1);
-		environ_copy = temp;
-		environ_copy[environ_copy_size] = new_var;
-		environ_copy[environ_copy_size + 1] = NULL;
+		glob.environ_copy[environ_copy_size] = new_var;
+		glob.environ_copy[environ_copy_size + 1] = NULL;
 		environ_copy_size++;
 	}
-	environ = environ_copy;
-
+	environ = glob.environ_copy;
 	return (0);
 }
 
 /**
-* unset_env - unset an environment variable
-* @var: variable to delete
-* Return: 0 (success), -1 (failure)
-*/
+ * unset_env - unset an environment variable
+ * @var: variable to delete
+ * Return: 0 (success), -1 (failure)
+ */
 int unset_env(char *var)
 {
-	char **environ_copy = copy_environ();
 	char *temp;
-	int i, var_len, environ_copy_size = get_env_size(environ_copy);
+	int i, var_len, environ_copy_size = get_env_size(glob.environ_copy);
 
 	if (!var && _strcon(var, '=') != 0)
 		return (-1);
 	var_len = _strlen(var);
-	for (i = 0; environ_copy[i] != NULL; i++)
+	for (i = 0; glob.environ_copy[i] != NULL; i++)
 	{
-		if (_strncmp(environ_copy[i], var, var_len) == 0 &&
-			environ_copy[i][var_len] == '=')
+		if (_strncmp(glob.environ_copy[i], var, var_len) == 0 &&
+			glob.environ_copy[i][var_len] == '=')
 		{
 			break;
 		}
@@ -94,12 +93,40 @@ int unset_env(char *var)
 
 	while (i <= environ_copy_size)
 	{
-		temp = environ_copy[i + 1];
-		environ_copy[i] = temp;
+		temp = glob.environ_copy[i + 1];
+		if (_strncmp(glob.environ_copy[i], var, var_len) == 0 &&
+			glob.environ_copy[i][var_len] == '=')
+			_free((void **)&glob.environ_copy[i]);
+		glob.environ_copy[i] = temp;
 		environ_copy_size--;
 		i++;
 	}
-	environ = environ_copy;
+	environ = glob.environ_copy;
 
+	return (0);
+}
+
+/**
+ * env_function_caller - calls env, setenv, and unsetenv
+ * @args: prompt tokens
+ * Return: 0 (Falsy) - 1 (Truthy)
+ */
+int env_function_caller(char **args)
+{
+	if (_strcmp(args[0], "env") == 0)
+	{
+		get_env();
+		return (1);
+	}
+	else if (_strcmp(args[0], "setenv") == 0)
+	{
+		set_env(args[1], args[2], 1);
+		return (1);
+	}
+	else if (_strcmp(args[0], "unsetenv") == 0)
+	{
+		unset_env(args[1]);
+		return (1);
+	}
 	return (0);
 }
