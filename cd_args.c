aed146f4219b4ab1b3debd7fcfb71995679169e4
LinMihigo <linmihigo@gmail.com>
2025-01-02 20:37:01 +0200

    Add option to check if a path exists before cd to it

--- cd_args.c
+++ cd_args.c
@@ -4,33 +4,43 @@
  * cd_args - Handles cd built-in command arguments
  * @args: Pointer to the cd command arguments
  * @envp: Pointer to environment variables
+ * @shell: name of the running shell
+ * @cmd_count: Count of commands entered in each shell session
  *
  * Return: 1 (success)
  */
 
-int cd_args(char **args, char **envp)
+int cd_args(char **args, char **envp, char *shell, int cmd_count)
 {
-	const char *path;
+	char *path = NULL;
 	char buf[1024];
-	int size;
 
 	(void)envp;
 	if (strcmp(args[1], "-") == 0)
 	{
-		size = oldpwd_path_size(envp);
-		path = get_oldpwd_path(envp, size);
+		path = get_oldpwd_path(envp, oldpwd_path_size(envp));
 		getcwd(buf, sizeof(buf));
 		setenv("OLDPWD", buf, 1);
 		fprintf(stdout, "%s\n", path);
 		chdir(path);
 		getcwd(buf, sizeof(buf));
 		setenv("PWD", buf, 1);
+		free(path);
 		return (1);
 	}
 	else
 	{
 		getcwd(buf, sizeof(buf));
 		setenv("OLDPWD", buf, 1);
+		if (access(args[1], F_OK) == 0)
+		{
+			path = args[1];
+		}
+		else
+		{
+			fprintf(stderr, "%s: %d: %s: can't cd to %s\n",
+			shell, cmd_count, args[0], args[1]);
+		}
 		path = args[1];
 		chdir(path);
 		getcwd(buf, sizeof(buf));
