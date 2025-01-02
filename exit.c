5229faa9e0fe891e52c4a44668b47d3346679f55
LinMihigo <linmihigo@gmail.com>
2025-01-02 20:42:54 +0200

    Make status part of glob struct

--- exit.c
+++ exit.c
@@ -9,19 +9,17 @@
 
 void exit_function(char *args[])
 {
-	int status;
-
 	if (args[1] != NULL)
-		status = atoi(args[1]);
-	else
-		status = 0;
+		glob.status = atoi(args[1]);
+
 	if (args != NULL)
 		free_resources(args);
 	if (glob.environ_copy)
 		free_resources(glob.environ_copy);
 	if (glob.input)
 		_free((void **)&glob.input);
-	exit(status);
+
+	exit(glob.status);
 }
 
 /**
